d8824ac6c8f44c30384077daf8e378f12e12b683
Lucas Coffee <lsterlingcoffee@gmail.com>
2019-12-11 19:30:35 -0800

    pseudocode plan of action for the javascript and some cleaning up of the html

---
+++ script.js
@@ -0,0 +1,69 @@
+var locationUserInput = $("#city-input");
+var locationInputButton = $("#city-input-button");
+var TodaysHumidity = $("#todays-humidity")
+var TodaysTemperature = $("#todays-temperature")
+var TodaysWindSpeed = $("#todays-wind-speed")
+var TodaysUvIndex = $("#todays-uv-index")
+var dayOneDisplayDiv = $("#day-one-display")
+var dayTwoDisplayDiv = $("#day-two-display")
+var dayThreeDisplayDiv = $("#day-three-display")
+var dayFourDisplayDiv = $("#day-four-display")
+var dayFiveDisplayDiv = $("#day-five-display")
+var mainCityInfoDiv = $("#main-city-info")
+var citySearchHistoryDiv = $("#previous-searches")
+var firstScreenLoadDiv = $("#first-screen-load")
+var weatherAfterSearchDiv = $("#weather-after-search")
+
+var citySearchHistory = [];
+var currentCitySearch = "";
+
+//Weather icon query setup
+
+// FUNCTIONS
+// Open the weather function with a clear function to clear out previous search info
+// reference local storage to get previous search info
+// assign previous cities to citySearchHistory
+  
+//if local storage is empty, leave the function
+//reversing the order so that the previous searches section is reverse order (so most recent at the top)
+//loop through the array
+
+
+//create a div with innerHTML = city
+//append that button to the section
+
+//event listener
+//getting the text from the button
+
+//if there are no previous searches, show the initial search screen and return. if there are previous searches, show the most recent search. and show this for all future searches.
+
+
+//assign todays date for weather data for today
+//converting temperature from kelvin to fahrenheit
+
+//attaching temperature to text
+//attaching humidity to text
+//attaching wind speed
+//attach UV Index - assigning lat and long for the UV Index
+//getting city name
+//get and setting today's date
+//Set the uv index for today. 
+
+//Set the weather data for five-day forecast
+
+// defining all the dates here
+
+// setting up date 1 for forecast
+// increasing date by 1 then setting day 2
+// increasing date by 1 then setting day 2
+// increasing date by 1 then setting day 2            
+// increasing date by 1 then setting day 2
+
+
+// formatting all the dates
+
+
+// get icons
+
+
+
